09eaae0def982c6383308ba948bdbd91f618d359
Stefans Mezulis <stefans.mezulis@gmail.com>
2023-04-08 12:05:09 +0100

    Ensure existing Faker methods can be called
    Before this change, existing Faker methods would fail once the
    `MarketDataProvider` had been added:
    
    ```python
    fake = Faker()
    fake.add_provider(MarketDataProvider)
    random_str = fake.lexify("???????")
    ```
    
    ```
    Traceback (most recent call last):
      File "faker_marketdata/tests/test_marketdata.py", line 74, in test_existing_fake_methods_still_work
        random_str = fake.lexify("???????")
                     ^^^^^^^^^^^^^^^^^^^^^^
      File "venv/lib/python3.11/site-packages/faker/providers/__init__.py", line 633, in lexify
        return _re_qm.sub(lambda x: self.random_element(letters), text)
               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "venv/lib/python3.11/site-packages/faker/providers/__init__.py", line 633, in <lambda>
        return _re_qm.sub(lambda x: self.random_element(letters), text)
                                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "venv/lib/python3.11/site-packages/faker/providers/__init__.py", line 545, in random_element
        return self.random_elements(elements, length=1)[0]
               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "venv/lib/python3.11/site-packages/faker/providers/__init__.py", line 493, in random_elements
        return fn(
               ^^^
      File "venv/lib/python3.11/site-packages/faker/utils/distribution.py", line 84, in choices_distribution
        uniform_sample = random_sample(random=random)
                         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "venv/lib/python3.11/site-packages/faker/utils/distribution.py", line 13, in random_sample
        return random.uniform(0.0, 1.0)
               ^^^^^^^^^^^^^^
    AttributeError: 'builtin_function_or_method' object has no attribute 'uniform'
    
    ```
    
    I've removed the `__init__` method from the `MarketDataProvider`
    because Faker will take care of initializing the provider when the class
    is passed to `add_provider`, and updated the provider to use the base
    class' random number generator.
    
    I've also reduced the number of iterations in the unit tests just to
    speed things up, but I'm happy to revert that if you think the
    extra iterations are necessary.

--- faker_marketdata/MarketDataProvider.py
+++ faker_marketdata/MarketDataProvider.py
@@ -1,17 +1,11 @@
 from faker.providers import BaseProvider
-from faker import Faker
-import random
 import pycountry
 
 _alphanumeric_alph = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 _alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 _cusip_alph = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ*@#'
 
-class MarketDataProvider (BaseProvider):
-
-    def __init__(self, seed=None):
-        BaseProvider.__init__(self, random.Random(seed))
-        self.rnd = random.Random(seed)
+class MarketDataProvider(BaseProvider):
 
     # Validation extracted from https://github.com/arthurdejong/python-stdnum/blob/master/stdnum/isin.py
     def isin(self):
@@ -19,9 +13,10 @@ class MarketDataProvider (BaseProvider):
         Generates a random, valid ISIN
         :return: a random ISIN with valid country code and validation digit
         """
+        rnd = self.generator.random
         country_codes = map(lambda x: x.alpha_2, list(pycountry.countries))
-        code = self.rnd.choice(list(country_codes)).upper()
-        rnd_str = "".join(self.rnd.choices(_alphanumeric_alph[11:], k=9))
+        code = rnd.choice(list(country_codes)).upper()
+        rnd_str = "".join(rnd.choices(_alphanumeric_alph[11:], k=9))
         number = code.upper() + rnd_str.zfill(9)
         number = ''.join(str(_alphanumeric_alph.index(n)) for n in number)
         number = ''.join( str((2, 1)[i % 2] * int(n)) for i, n in enumerate(reversed(number)))
@@ -33,7 +28,8 @@ class MarketDataProvider (BaseProvider):
         Generates a random, valid SEDOL
         :return: SEDOL as a string
         """
-        str_part = "".join(self.rnd.choices(_alphanumeric_alph[11:], k=6))
+        rnd = self.generator.random
+        str_part = "".join(rnd.choices(_alphanumeric_alph[11:], k=6))
         weights = (1, 3, 1, 7, 3, 9)
         s = sum(w * _alphanumeric_alph.index(n) for w, n in zip(weights, str_part))
         number =  str((10 - s) % 10)
@@ -44,19 +40,22 @@ class MarketDataProvider (BaseProvider):
         Generates a random MIC
         :return: MIC as a string
         """
-        return "".join(self.rnd.choices(_alphanumeric_alph[10:], k=4))
+        rnd = self.generator.random
+        return "".join(rnd.choices(_alphanumeric_alph[10:], k=4))
 
     def lei(self):
-        lou = "".join(self.rnd.choices(_alphanumeric_alph, k=4))
+        rnd = self.generator.random
+        lou = "".join(rnd.choices(_alphanumeric_alph, k=4))
         reserved = "00"
-        org_id = "".join(self.rnd.choices(_alphanumeric_alph, k=12))
+        org_id = "".join(rnd.choices(_alphanumeric_alph, k=12))
         partial_lei = "{}{}{}".format(lou, reserved, org_id)
         checksum = sum(list(map(lambda x: _alph.index(x) if x in _alph else 0, partial_lei))) % 98
         checksum = 98 - int(checksum)
         return "{}{}".format(partial_lei, checksum)
 
     def cusip(self):
-        base_id = "".join(self.rnd.choices(_cusip_alph, k=8))
+        rnd = self.generator.random
+        base_id = "".join(rnd.choices(_cusip_alph, k=8))
         check_char = ''.join(str((1, 2)[i % 2] * _cusip_alph.index(n)) for i, n in enumerate(base_id))
         check_char = str((10 - sum(int(n) for n in check_char)) % 10)
         return "{}{}".format(base_id, check_char)
@@ -66,8 +65,9 @@ class MarketDataProvider (BaseProvider):
         Generates a random RIC string
         :return: a RIC with the format {three chars} {dot} {two chars}
         """
-        ticker = "".join(self.rnd.choices(_alph, k=3))
-        exchange = "".join(self.rnd.choices(_alph, k=2))
+        rnd = self.generator.random
+        ticker = "".join(rnd.choices(_alph, k=3))
+        exchange = "".join(rnd.choices(_alph, k=2))
         return "{}.{}".format(ticker, exchange)
 
     def ticker (self):
@@ -75,30 +75,34 @@ class MarketDataProvider (BaseProvider):
         Generates a random 4-char ticker
         :return: a random ticker ID
         """
-        ticker = "".join(self.rnd.choices(_alph, k=4))
+        rnd = self.generator.random
+        ticker = "".join(rnd.choices(_alph, k=4))
         return "{}".format(ticker)
 
     def nsin (self):
-        code  = "".join(self.rnd.choices(_alphanumeric_alph, k=9))
+        rnd = self.generator.random
+        code  = "".join(rnd.choices(_alphanumeric_alph, k=9))
         return "{}".format(code)
 
     def figi (self):
-        head = "B" # """.join(self.rnd.choices(_alph, k=1))
-        char2 = self.rnd.choices(_alph)[0]
+        rnd = self.generator.random
+        head = "B" # """.join(rnd.choices(_alph, k=1))
+        char2 = rnd.choices(_alph)[0]
         if head == "B":
-            char2 = self.rnd.choice([c for c in _alph if c not in ['S','M']])
+            char2 = rnd.choice([c for c in _alph if c not in ['S','M']])
         elif head == "G":
-            char2 = self.rnd.choice([c for c in _alph if c not in ['G','B', 'H']])
+            char2 = rnd.choice([c for c in _alph if c not in ['G','B', 'H']])
         elif head == "K":
-            char2 = self.rnd.choice([c for c in _alph if c != 'Y'])
+            char2 = rnd.choice([c for c in _alph if c != 'Y'])
         elif head == "V":
-            char2 = self.rnd.choice([c for c in _alph if c != 'G'])
+            char2 = rnd.choice([c for c in _alph if c != 'G'])
         char3 = "G"
-        tail = "".join(self.rnd.choices(_alphanumeric_alph, k=7))
+        tail = "".join(rnd.choices(_alphanumeric_alph, k=7))
         id = "{}{}{}{}".format(head, char2, char3, tail)
         number = ''.join(str(_alphanumeric_alph.index(n) * (1, 2)[i % 2]) for i, n in enumerate(id[:11]))
         number = str((10 - sum(int(n) for n in number)) % 10)
         return "{}{}".format(id, number)
 
     def marketType (self):
-        return self.rnd.choice(["bond", "commodity", "derivatives", "forex", "otc", "pe", "spot", "stock"])
+        rnd = self.generator.random
+        return rnd.choice(["bond", "commodity", "derivatives", "forex", "otc", "pe", "spot", "stock"])
--- tests/test_marketdata.py
+++ tests/test_marketdata.py
@@ -1,3 +1,5 @@
+from faker import Faker
+
 from faker_marketdata import MarketDataProvider
 
 import unittest
@@ -6,64 +8,78 @@ import unittest
 class MarketDataTests(unittest.TestCase):
 
     def setUp(self):
-        self.md = MarketDataProvider()
+        self.md = Faker()
+        self.md.add_provider(MarketDataProvider)
 
     def test_figi(self):
-        figis = [self.md.figi() for _ in range(10000)]
+        figis = [self.md.figi() for _ in range(100)]
         self.assertTrue(all(figi for figi in figis))
 
     def test_isin(self):
-        isins = [self.md.isin() for _ in range(10000)]
+        isins = [self.md.isin() for _ in range(100)]
         self.assertTrue(all(isin for isin in isins))
 
     def test_lei(self):
-        leis = [self.md.lei() for _ in range(10000)]
+        leis = [self.md.lei() for _ in range(100)]
         self.assertTrue(all(lei for lei in leis))
 
     def test_market_type(self):
-        mt = [self.md.marketType() for _ in range(10000)]
+        mt = [self.md.marketType() for _ in range(100)]
         self.assertTrue(all(marketType for marketType in mt))
 
     def test_mic(self):
-        mics = [self.md.mic() for _ in range(10000)]
+        mics = [self.md.mic() for _ in range(100)]
         self.assertTrue(all(mic for mic in mics))
 
     def test_nsin(self):
-        nsins = [self.md.nsin() for _ in range(10000)]
+        nsins = [self.md.nsin() for _ in range(100)]
         self.assertTrue(all(nsin for nsin in nsins))
 
     def test_ric(self):
-        rics = [self.md.ric() for _ in range(10000)]
+        rics = [self.md.ric() for _ in range(100)]
         self.assertTrue(all(ric for ric in rics))
 
     def test_cusip (self):
-        cusips = [self.md.cusip() for _ in range(10000)]
+        cusips = [self.md.cusip() for _ in range(100)]
         self.assertTrue(all(cusip for cusip in cusips))
 
     def test_sedol(self):
-        sedols = [self.md.sedol() for _ in range(10000)]
+        sedols = [self.md.sedol() for _ in range(100)]
         self.assertTrue(all(sedol for sedol in sedols))
 
     def test_tickers(self):
-        tickers = [self.md.ticker() for _ in range(10000)]
+        tickers = [self.md.ticker() for _ in range(100)]
         self.assertTrue(all(ticker for ticker in tickers))
 
+
+class FakerIntegrationTests(unittest.TestCase):
     """
     Tests integration with Faker
     """
     def test_faker_provider(self):
-        from faker import Faker
         fake = Faker()
-        fake.add_provider(MarketDataProvider())
+        fake.add_provider(MarketDataProvider)
         self.assertTrue(fake.sedol())
 
-    def test_seed_fixes_random (self):
-        from faker import Faker
+    def test_seed_fixes_random(self):
         faker1 = Faker()
         faker2 = Faker()
-        faker1.add_provider(MarketDataProvider(seed=23))
-        faker2.add_provider(MarketDataProvider(seed=23))
-        self.assertEqual(faker1.sedol(), faker2.sedol())
+        faker1.add_provider(MarketDataProvider)
+        faker2.add_provider(MarketDataProvider)
+
+        Faker.seed(123)
+        sedol1 = faker1.sedol()
+        Faker.seed(123)
+        sedol2 = faker2.sedol()
+        self.assertEqual(sedol1, sedol2)
+
+    def test_existing_fake_methods_still_work(self):
+        fake = Faker()
+        fake.add_provider(MarketDataProvider)
+        sedol = fake.sedol()
+        self.assertTrue(sedol)
+        random_str = fake.lexify("???????")
+        self.assertTrue(random_str)
 
 
 if __name__ == '__main__':
