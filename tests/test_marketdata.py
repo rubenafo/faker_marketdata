93f2a5b6c6a89976bfd4382afb5171db3dce501a
Stefans Mezulis <stefans.mezulis@gmail.com>
2023-04-12 07:34:29 +0100

    Restore original number of iterations

--- tests/test_marketdata.py
+++ tests/test_marketdata.py
@@ -12,43 +12,43 @@ class MarketDataTests(unittest.TestCase):
         self.md.add_provider(MarketDataProvider)
 
     def test_figi(self):
-        figis = [self.md.figi() for _ in range(100)]
+        figis = [self.md.figi() for _ in range(10000)]
         self.assertTrue(all(figi for figi in figis))
 
     def test_isin(self):
-        isins = [self.md.isin() for _ in range(100)]
+        isins = [self.md.isin() for _ in range(10000)]
         self.assertTrue(all(isin for isin in isins))
 
     def test_lei(self):
-        leis = [self.md.lei() for _ in range(100)]
+        leis = [self.md.lei() for _ in range(10000)]
         self.assertTrue(all(lei for lei in leis))
 
     def test_market_type(self):
-        mt = [self.md.marketType() for _ in range(100)]
+        mt = [self.md.marketType() for _ in range(10000)]
         self.assertTrue(all(marketType for marketType in mt))
 
     def test_mic(self):
-        mics = [self.md.mic() for _ in range(100)]
+        mics = [self.md.mic() for _ in range(10000)]
         self.assertTrue(all(mic for mic in mics))
 
     def test_nsin(self):
-        nsins = [self.md.nsin() for _ in range(100)]
+        nsins = [self.md.nsin() for _ in range(10000)]
         self.assertTrue(all(nsin for nsin in nsins))
 
     def test_ric(self):
-        rics = [self.md.ric() for _ in range(100)]
+        rics = [self.md.ric() for _ in range(10000)]
         self.assertTrue(all(ric for ric in rics))
 
     def test_cusip (self):
-        cusips = [self.md.cusip() for _ in range(100)]
+        cusips = [self.md.cusip() for _ in range(10000)]
         self.assertTrue(all(cusip for cusip in cusips))
 
     def test_sedol(self):
-        sedols = [self.md.sedol() for _ in range(100)]
+        sedols = [self.md.sedol() for _ in range(10000)]
         self.assertTrue(all(sedol for sedol in sedols))
 
     def test_tickers(self):
-        tickers = [self.md.ticker() for _ in range(100)]
+        tickers = [self.md.ticker() for _ in range(10000)]
         self.assertTrue(all(ticker for ticker in tickers))
 
 
